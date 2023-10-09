047c165116b20bf92304cd05fc2ced9f8df83d97
Da Silva Mendonca Thomas <thomas.dasilvamendonca@protonmail.com>
2023-10-09 15:34:00 +0200

    fix Zendesk users list

--- Loop/ZendeskUsersLoop.php
+++ Loop/ZendeskUsersLoop.php
@@ -9,22 +9,25 @@ use Thelia\Core\Template\Element\LoopResult;
 use Thelia\Core\Template\Element\LoopResultRow;
 use Thelia\Core\Template\Loop\Argument\Argument;
 use Thelia\Core\Template\Loop\Argument\ArgumentCollection;
+use Thelia\Model\Customer;
+use Thelia\Model\CustomerQuery;
 use ZenDesk\Utils\ZenDeskManager;
 
 class ZendeskUsersLoop extends BaseLoop implements ArraySearchLoopInterface
 {
-    public function parseResults(LoopResult $loopResult)
+    public function parseResults(LoopResult $loopResult): LoopResult
     {
         foreach ($loopResult->getResultDataCollection() as $item) {
 
             $loopResultRow = new LoopResultRow();
 
-            $loopResultRow->set("NAME", $item[0]);
-            $loopResultRow->set("EMAIL", $item[1]);
-            $loopResultRow->set("ROLE", $item[2]);
-            $loopResultRow->set("CREATED_AT", $item[3]);
-            $loopResultRow->set("UPDATE_AT", $item[4]);
-            $loopResultRow->set("LOCALE", $item[5]);
+            $loopResultRow->set("ID", $item[0]);
+            $loopResultRow->set("NAME", $item[1]);
+            $loopResultRow->set("EMAIL", $item[2]);
+            $loopResultRow->set("ROLE", $item[3]);
+            $loopResultRow->set("CREATED_AT", $item[4]);
+            $loopResultRow->set("UPDATE_AT", $item[5]);
+            $loopResultRow->set("LOCALE", $item[6]);
 
             $loopResult->addRow($loopResultRow);
         }
@@ -33,30 +36,45 @@ class ZendeskUsersLoop extends BaseLoop implements ArraySearchLoopInterface
 
     }
 
-    public function buildArray()
+    public function buildArray(): array
     {
         $items = [];
 
         $manager = new ZenDeskManager();
         $zendeskUsers = $manager->getAllUsers();
 
-        foreach ($zendeskUsers as $user)
+        foreach ($zendeskUsers as $usersArray)
         {
-            if ($this->getEmail() === $user->email)
+            foreach ($usersArray as $user)
             {
-                $tmpItems = [];
-                $tmpItems[] = $user->name;
-                $tmpItems[] = $user->email;
-                $tmpItems[] = $user->role;
-                $tmpItems[] = $this->getFormatDate(strtotime($user->created_at), $this->getLocale());
-                $tmpItems[] = $this->getFormatDate(strtotime($user->updated_at), $this->getLocale());
-                $tmpItems[] = $user->locale;
-
-                $items[] = $tmpItems;
+                if ($customer = $this->getCustomerByEmail($user->email))
+                {
+                    $items[] = [
+                        $customer->getId(),
+                        $user->name,
+                        $user->email,
+                        $user->role,
+                        $this->getFormatDate(strtotime($user->created_at), $this->getLocale()),
+                        $this->getFormatDate(strtotime($user->updated_at), $this->getLocale()),
+                        $user->locale
+                    ];
+                }
             }
         }
 
+        sort($items);
+
         return $items;
+    }
+
+    public function getCustomerByEmail($email): ?Customer
+    {
+        if ($email === null)
+        {
+            return null;
+        }
+
+        return CustomerQuery::create()->findOneByEmail($email);
     }
 
     private function getFormatDate($datetime, $locale): bool|string
